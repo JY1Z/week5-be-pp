51563e6199745593e43e3b000d2f3d5f7430104e
Ke Jia <ke.jia@metropolia.fi>
2024-09-16 15:14:52 +0300

    Implementing Error-Handling Middleware

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
-const { unknownEndpoint } = require("./middleware/customMiddleware");
+const { unknownEndpoint, errorHandler } = require("./middleware/customMiddleware");
 
 connectDB();
 
@@ -20,10 +20,22 @@ app.use("/api/tours", tourRouter);
 // Use the userRouter for all /users routes
 app.use("/api/users", userRouter);
 
+// Example route that throws an error
+app.get('/error', (req, res, next) => {
+  // Trigger an error
+  const error = new Error("Network problem");
+  next(error);
+});
+
+// Use the unknownEndpoint middleware for handling undefined routes
+
+// Use the errorHandler middleware for handling errors
+app.use(errorHandler);
+
 app.use(unknownEndpoint);
-// app.use(errorHandler);
 
 const port = process.env.PORT || 4000;
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
